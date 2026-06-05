683839e89e3e38cc8ee376ab7c910d2d43d49c46
aashishjha174-arch <aashishjha174@gmail.com>
2026-06-05 16:35:49 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/secret-garden';
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://gardenrestaurant.netlify.app/', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
