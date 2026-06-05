ed972b7bd6c6db3eda1eaebd5606fd6e959ee0f0
aashishjha174-arch <aashishjha174@gmail.com>
2026-06-05 16:37:16 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/secret
 
 // Middleware
 app.use(cors({
-  origin: ['https://gardenrestaurant.netlify.app/', 'http://localhost:3000'],
+  origin: ['https://gardenrestaurant.netlify.app', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
